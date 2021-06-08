0d68a03eadaad8968af448ccf70047b6379d81ed
HaoZhang <h.zhang@smail.nju.edu.cn>
2021-06-08 14:50:21 +0800

    post diy-starter code

---
+++ diy-spring-boot-starter/hello-spring-boot-starter-autoconfigurer/src/main/java/com/zhao/HelloSpringBootStarterAutoconfigurerApplication.java
@@ -0,0 +1,13 @@
+package com.zhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootStarterAutoconfigurerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootStarterAutoconfigurerApplication.class, args);
+    }
+
+}
---
+++ diy-spring-boot-starter/hello-spring-boot-starter-test/src/main/java/com/zhao/HelloSpringBootStarterTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootStarterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootStarterTestApplication.class, args);
+    }
+
+}
